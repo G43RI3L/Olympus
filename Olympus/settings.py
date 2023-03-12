c055171ad8099c29f729677d997f9845cb7183d0
Gabriel Higor <127054780+G43RI3L@users.noreply.github.com>
2023-03-12 17:17:48 -0300

    Update settings.py

--- Olympus/settings.py
+++ Olympus/settings.py
@@ -20,13 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dem#66np^4muotdy+raegjx15h_k#0%y&6v+td@v0duct8qs1!'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
